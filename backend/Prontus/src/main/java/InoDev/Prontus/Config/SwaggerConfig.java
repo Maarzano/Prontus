422b656e31511cfdbfd02c6aabd03f93570902f3
Marzano <arthuramarzano10@gmail.com>
2025-04-29 11:16:40 -0300

    configurando swagger

---
+++ backend/Prontus/src/main/java/InoDev/Prontus/Config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package InoDev.Prontus.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API do Meu Sistema", version = "1.0", description = "Documentação da API"))
+public class SwaggerConfig {
+//http://localhost:8080/swagger-ui/index.html
+}
