f7bb179177809c35d07ac8410f9cba653b054a17
Marzano <arthuramarzano10@gmail.com>
2025-05-06 12:35:59 -0300

    Update FXML layout and module dependencies for improved functionality

--- frontend/prontus/src/main/java/module-info.java
+++ frontend/prontus/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module inodev {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
 
     opens inodev to javafx.fxml;
     exports inodev;
