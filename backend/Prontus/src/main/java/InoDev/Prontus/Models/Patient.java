c7918a3c717915f47016c9c6bc9d7d9722a8576f
Arthur Alves <alves2araujo@gmail.com>
2025-05-06 18:24:21 -0300

    Colocando ColumnDefinition em campos necessários

--- backend/Prontus/src/main/java/InoDev/Prontus/Models/Patient.java
+++ backend/Prontus/src/main/java/InoDev/Prontus/Models/Patient.java
@@ -3,6 +3,7 @@ package InoDev.Prontus.Models;
 import java.sql.Date;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,13 +23,14 @@ public class Patient {
     @Column(name = "patient_name", nullable = false, unique = false, length = 100)
     private String name;
 
-    @Column(name = "patient_cpf", nullable = false, unique = true, length = 14)
+    @Column(name = "patient_cpf", nullable = false, unique = true, columnDefinition = "CHAR(11)")
     private String cpf;
 
     @Column(name = "patient_birthday", nullable = true, unique = false)
+    @Past(message = "A data de nascimento precisa ser no passado!")
     private Date dataNasc;
 
-    @Column(name = "patient_cellphone", nullable = true, length = 15)
+    @Column(name = "patient_cellphone", nullable = true, columnDefinition = "CHAR(11)")
     private String cellphone;
 
     @Column(name = "patient_email", nullable = true, unique = true, length = 100)
