926b63f6d2d096da5268fdbda97d80ea122be4d1
Marzano <arthuramarzano10@gmail.com>
2025-05-06 11:36:06 -0300

    Add logout functionality to main controller classes and update FXML files

---
+++ frontend/prontus/src/main/java/inodev/controllers/TelaPrincipalAdmSupremoController.java
@@ -0,0 +1,22 @@
+package inodev.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TelaPrincipalAdmSupremoController {
+
+    @FXML
+    public void handleLogout(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/inodev/login.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
